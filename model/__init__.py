4d77e2c721b198fb2afd69e1f03e1771fbdc805a
yustas147 <yustas@simbioz.com.ua>
2018-01-19 00:43:21 +0200

    validation removed

--- model/__init__.py
+++ model/__init__.py
@@ -1,2 +1,2 @@
 #from . import stock_inventory_line, stock_picking
-from . import lcb_base, stock_inventory_line, sale_order_line, stock_picking, pos_order_line, stock_production_lot, product_product
+from . import lcb_base, stock_inventory_line, sale_order_line, stock_picking, pos_order_line, product_product
