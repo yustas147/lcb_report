81c92e1f22e59334b8b0392b82b296897aa9a468
yustas147 <yustas@simbioz.com.ua>
2018-02-06 16:16:48 +0200

    fixed Total price and Total Excise tax

--- model/lcb_base.py
+++ model/lcb_base.py
@@ -15,6 +15,9 @@ _logger = logging.getLogger(__name__)
 class lcb_base(models.Model):
     _name='lcb.base'
     
+    _sales_tax = 0.076
+    _marijuana_excise_tax = 0.37
+    _total_tax = _marijuana_excise_tax + _sales_tax
     #@api.multi
     #def get_tax_amount(self):
         #for inst in self:
@@ -35,43 +38,28 @@ class lcb_base(models.Model):
         for inst in self:
             inst.license_number = company.api_license_number
             
-    
-    @api.depends('product_price_no_tax')
+    @api.multi
     def _get_tax_amount(self):
-        tax_marijuana_037 = 0.37
         for inst in self:
             ######################################## eto kostyl vremenno
-#            inst.tax_amount = inst.price_tax - inst.price_subtotal*0.076
-            inst.tax_amount =  inst.product_price_no_tax * tax_marijuana_037
-#            inst.tax_amount = (inst.price_subtotal_incl - inst.price_subtotal)*1.084    
-#            inst.tax_amount = inst.price_subtotal_incl - inst.price_subtotal*1.084    
-
-    @api.multi
-    def _get_product_price_no_tax(self):
-        total_tax_coeff = 0.37 + 0.076
-        for inst in self:
-            PT = inst.price_subtotal_incl * total_tax_coeff
-            inst.prduct_price_no_tax = inst.price_subtotal_incl - PT 
-            
-    #@api.multi
-    #def _get_pp_tax(self):
-        #tax_marijuana_037 = 0.37
-        #sales_tax_0076 = 0.076
-        #total_tax_coeff = tax_marijuana_037 + sales_tax_0076
-        #for inst in self:
-            #PT = inst.price_subtotal_incl * total_tax_coeff
-            #inst.prduct_price_no_tax = inst.price_subtotal_incl - PT 
-            #inst.tax_amount =  inst.product_price_no_tax * tax_marijuana_037
-             
-            
+            inst.tax_amount = inst.price_subtotal_incl - inst.price_subtotal*1.084    
 
     
+    @api.multi
+    def _get_pp(self):
+        for inst in self:
+            inst.pp = inst.price_subtotal_incl/(1+self._total_tax)
+            
+    @api.multi
+    def _get_tet(self):
+        for inst in self:
+            inst.tet = inst.pp * self._marijuana_excise_tax
+        
     license_number = fields.Char(string="License #", compute='_get_license_number')
     is_medical = fields.Boolean(string="Medical Sale?", compute='_check_med', store=False)
-    #tax_amount = fields.Float(string="Total Excise Tax", compute='_get_pp_tax', digits=None, store=True)
-    #product_price_no_tax = fields.Float(string="Total Price", compute='_get_pp_tax', digits=None, store=True)
-    tax_amount = fields.Float(string="Total Excise Tax", compute='_get_tax_amount',  store=False)
-    product_price_no_tax = fields.Float(string="Total Price", compute='_get_product_price_no_tax', store=False)
+    tax_amount = fields.Float(string="Total Excise Tax", compute='_get_tax_amount', digits=None)
+    pp = fields.Float(string='Total price', compute='_get_pp', digits=None)
+    tet = fields.Float(string='Total Excise Tax', compute='_get_tet', digits=None)
     #tax_amount = fields.Float(string="Total Excise Tax", compute='get_tax_amount', digits=None)
     #sale_date = fields.Datetime(string="Sale Date", related='order_id.date_order', store=True)
     
